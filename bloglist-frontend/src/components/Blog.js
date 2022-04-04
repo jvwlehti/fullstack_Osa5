3600d48dabe2ae36a1e50b6c10128f38aa22973a
jvwlehti <93128297+jvwlehti@users.noreply.github.com>
2022-04-04 13:30:11 +0300

    Tehtävä 5.7
    tehty t 5.7

--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,29 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+const Blog = ({ blog }) => {
+  const [show, setShow] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={() => setShow(previousShow => !previousShow)}>{show ? 'hide' : 'view'}</button>
+      </div>
+      <div>
+        {show && (<div>
+          <div>{blog.url}</div>
+          <div>likes {blog.likes} <button>like</button></div>
+          <div>{blog.user.name}</div>
+        </div>)}
+      </div>
+    </div>)
+}
 
 export default Blog
