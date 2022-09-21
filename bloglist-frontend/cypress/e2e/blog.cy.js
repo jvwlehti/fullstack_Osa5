bf2efc48a63ee853cab4df926d7be7bb91b93900
jvwlehti <93128297+jvwlehti@users.noreply.github.com>
2022-09-21 10:54:03 +0300

    tehtävät 5.17-5.19

---
+++ bloglist-frontend/cypress/e2e/blog.cy.js
@@ -0,0 +1,38 @@
+describe('Blog app', function() {
+  beforeEach(function() {
+    cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    const user = {
+      name: 'Matti Luukkainen',
+      username: 'mluukkai',
+      password: 'salainen'
+    }
+    cy.request('POST', 'http://localhost:3001/api/users/', user)
+    cy.visit('http://localhost:3000')
+  })
+
+  it('Login form is shown', function() {
+    cy.contains('Log in to application')
+  })
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.contains('login').click()
+      cy.get('#username').type('mluukkai')
+      cy.get('#password').type('salainen')
+      cy.get('#login-button').click()
+      cy.contains('Matti Luukkainen logged in')
+    })
+
+    it.only('fails with wrong credentials', function() {
+      cy.contains('login').click()
+      cy.get('#username').type('mluukkai')
+      cy.get('#password').type('wrong')
+      cy.get('#login-button').click()
+
+      cy.get('.error')
+        .should('contain', 'wrong credentials')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+        .and('have.css', 'border-style', 'solid')
+    })
+  })
+})
--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -115,6 +115,7 @@ const App = () => {
           <div>
             username
             <input
+              id='username'
               type="text"
               value={username}
               name="Username"
@@ -124,13 +125,14 @@ const App = () => {
           <div>
             password
             <input
+              id='password'
               type="password"
               value={password}
               name="Password"
               onChange={({ target }) => setPassword(target.value)}
             />
           </div>
-          <button type="submit">login</button>
+          <button id='login-button' type="submit">login</button>
         </form>
       </div>
     )
